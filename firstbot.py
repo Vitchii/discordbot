32a0e5e265b3be2f184ba43d31ebd10000bf8cf5
nilslambertz <nilslambertz@web.de>
2020-04-22 19:55:03 +0200

    small fixes

--- firstbot.py
+++ firstbot.py
@@ -21,7 +21,7 @@ faecherarray = [automatenObj, rechnernetzeObj, kleinesProjektObj, grossesProjekt
 
 @client.event
 async def on_message(m):
-    if m.author.bot or m.channel.name != "info-bot":
+    if m.author.bot or (m.channel.name != "info-bot" and m.channel.name != "botkanal"):
         return
 
     r = True
